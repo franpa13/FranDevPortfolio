3271bb5cd7a0036041ffd2519f6507e8df9245ed
franpa13 <frandelregue@gmail.com>
2024-07-02 16:41:44 -0300

    arreglando estilos

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,158 +1,192 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Redes from './Redes/Redes';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import "./Navbar.css"
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Redes from "./Redes/Redes";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import "./Navbar.css";
 
-import AdbIcon from '@mui/icons-material/Adb';
-import img from "/src/assets/ParedesFrancisco.jpeg"
-import { Link } from 'react-router-dom';
+import AdbIcon from "@mui/icons-material/Adb";
+import img from "/src/assets/ParedesFrancisco.jpeg";
+import { Link } from "react-router-dom";
 const pages = [
-    {
-        id: "/",
-        text: 'Sobre mi'
-    },
-    {
-        id: "/educacion",
-        text: 'Educacion/Skills'
-    },
+  {
+    id: "/",
+    text: "Sobre mi",
+  },
+  {
+    id: "/educacion",
+    text: "Educacion/Skills",
+  },
 
-    {
-        text: 'Proyectos',
-        id: "/proyectos"
-    },
+  {
+    text: "Proyectos",
+    id: "/proyectos",
+  },
 ];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    return (
+  return (
+    <AppBar position="static" className="bar fixed top-0 w-full z-50 ">
+      <div maxWidth="xl" className="pr-0 pl-0 px-0 bar animate-blurred-fade-in">
+        <Toolbar
+          className="flex justify-center items-center p-0 pr-0 pl-0 "
+          disableGutters
+        >
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <div>
+                <a href="#inicio">
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography
+                      textAlign="center"
+                      className="text-black  w-full  "
+                    >
+                      Inicio
+                    </Typography>
+                  </MenuItem>
+                </a>
+                <a href="#proyectos">
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography
+                      textAlign="center"
+                      className="text-black  w-full   "
+                    >
+                      Proyectos
+                    </Typography>
+                  </MenuItem>
+                </a>
+                <a href="#educacion">
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography
+                      textAlign="center"
+                      className="text-black  w-full  "
+                    >
+                      Educacion/Skills
+                    </Typography>
+                  </MenuItem>
+                </a>
+              </div>
+            </Menu>
+          </Box>
 
-        <AppBar position="static" className="bar fixed top-0 w-full z-50 " >
-            <div maxWidth="xl" className='pr-0 pl-0 px-0 bar animate-blurred-fade-in'>
-                <Toolbar className='flex justify-center items-center p-0 pr-0 pl-0 ' disableGutters >
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", gap:"8px" } }}>
+            <a href={`#inicio`}>
+              <Button
+                size="small"
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  mx: 2,
+                  fontWeight: "bold",
+                  "&:hover": { backgroundColor: "#38bdf8", color: "black" },
+                  textAlign: "center",
+                  border: "1px solid #38bdf8",
+                  color: "white",
+                  display: "block",
+                }}
+              >
+                Inicio
+              </Button>
+            </a>
+            <a href={`#proyectos`}>
+              <Button
+                size="small"
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  mx: 2,
+                  fontWeight: "bold",
+                  "&:hover": { backgroundColor: "#38bdf8", color: "black" },
+                  textAlign: "center",
+                  border: "1px solid #38bdf8",
+                  color: "white",
+                  display: "block",
+                }}
+              >
+                Proyectos
+              </Button>
+            </a>
+            <a href={`#educacion`}>
+              <Button
+                size="small"
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  mx: 2,
+                  fontWeight: "bold",
+                  "&:hover": { backgroundColor: "#38bdf8",color: "black"},
+                  textAlign: "center",
+                  border: "1px solid #38bdf8",
+                  color: "white",
+                  display: "block",
+                }}
+              >
+                Educacion/Skills
+              </Button>
+            </a>
+          </Box>
 
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            <div>
-
-                                <a href="#inicio">
-
-                                    <MenuItem onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center" className='text-black  w-full  ' >Inicio</Typography>
-                                    </MenuItem>
-                                </a>
-                                <a href="#proyectos">
-
-                                    <MenuItem onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center" className='text-black  w-full   ' >Proyectos</Typography>
-                                    </MenuItem>
-                                </a>
-                                <a href="#educacion">
-
-                                    <MenuItem onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center" className='text-black  w-full  ' >Educacion/Skills</Typography>
-                                    </MenuItem>
-                                </a>
-                            </div>
-
-                        </Menu>
-                    </Box>
-
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-
-                        <a href={`#inicio`}>
-                            <Button
-
-                                onClick={handleCloseNavMenu}
-
-                                sx={{ my: 2, mx: 2, fontWeight: "bold" ,'&:hover': {backgroundColor:"#38bdf8", color:"#464545"},textAlign: "center",border:"1px solid #38bdf8", color: 'white', display: 'block' }}
-                            >
-                                Inicio
-                            </Button>
-                        </a>
-                        <a href={`#proyectos`}>
-                            <Button
-
-                                onClick={handleCloseNavMenu}
-
-                                sx={{ my: 2, mx: 2, fontWeight: "bold", '&:hover': {backgroundColor:"#38bdf8", color:"#464545"},textAlign: "center", border:"1px solid #38bdf8", color: 'white', display: 'block' }}
-                            >
-                                Proyectos
-                            </Button>
-                        </a>
-                        <a href={`#educacion`}>
-                            <Button
-
-                                onClick={handleCloseNavMenu}
-
-                                sx={{ my: 2, mx: 2, fontWeight: "bold", '&:hover': {backgroundColor:"#38bdf8", color:"#464545"},textAlign: "center" , border:"1px solid #38bdf8",color: 'white', display: 'block' }}
-                            >
-                                Educacion/Skills
-                            </Button>
-                        </a>
-
-                    </Box>
-
-                    <Box className="flex w-full justify-end md:w-auto md:justify-center  md:gap-12 " sx={{ flexGrow: 0 }}>
-                        <Redes  ></Redes>
-                        {/* <Tooltip title="!Hola¡" >
+          <Box
+            className="flex w-full justify-end md:w-auto md:justify-center  md:gap-12 "
+            sx={{ flexGrow: 0 }}
+          >
+            <Redes></Redes>
+            {/* <Tooltip title="!Hola¡" >
 
                             <Avatar
                                 variant='rounded'
@@ -165,12 +199,10 @@ function ResponsiveAppBar() {
                             />
 
                         </Tooltip> */}
-
-                    </Box>
-                </Toolbar>
-            </div>
-        </AppBar >
-
-    );
+          </Box>
+        </Toolbar>
+      </div>
+    </AppBar>
+  );
 }
 export default ResponsiveAppBar;
